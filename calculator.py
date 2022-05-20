e8f313be52ea4f581069c5884296ecb4d3029c12
Agnes-Maiza <89739712+Agnes-Maiza@users.noreply.github.com>
2022-05-20 11:29:51 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#complex calculator
+x=input("Enter the first value ")
+y=input("Enter the second value ")
+p=input("Enter an operator.(eg +,-,*,/,%) ")
+if p == '+':
+    result= int (x)+int (y)
+    print(x,p,y,"=",result)
+elif p == '-':
+    result= int (x)-int (y)
+    print(x,p,y,"=",result)
+elif p == '*':
+    result= int (x)*int (y)
+    print(x,p,y,"=",result)
+elif p == '/':
+    result= int (x)/int (y)
+    print(x,p,y,"=",result)
+elif p == '%':
+    
+    result= int (x)%int (y)
+    print(x,p,y,"=",result)
+else :
+    print("Invalid operator")
+    
